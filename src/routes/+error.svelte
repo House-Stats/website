efd4f6d3e922170c617298cd8addb9c4c156463f
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-01-30 23:01:54 +0000

    Typo

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,7 @@
 <div class="flex h-screen justify-center items-center">
     <div class="text-center">
         <p class="text-2xl">{$page.error.message}</p>
-        <p class="text-lg">This site is still under developemnt so some features may be missing</p>
+        <p class="text-lg">This site is still under development so some features may be missing</p>
     </div>
 
 </div>
