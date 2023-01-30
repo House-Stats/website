cf7eebcf6eec1c818b7f3ca3a8b89daa26096a75
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-01-30 22:35:48 +0000

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { page } from '$app/stores';
+  </script>
+  
+<div class="flex h-screen justify-center items-center">
+    <div class="text-center">
+        <p class="text-2xl">{$page.error.message}</p>
+        <p class="text-lg">This site is still under developemnt so some features may be missing</p>
+    </div>
+
+</div>
