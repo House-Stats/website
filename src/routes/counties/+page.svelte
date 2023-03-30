d3ff41bba5f8f9387cb51639e516bcc90da7b623
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-03-30 19:25:17 +0100

    Coming soon message

--- src/routes/counties/+page.svelte
+++ src/routes/counties/+page.svelte
@@ -3,3 +3,4 @@
 	<title>House Stats | Counties</title>
 </svelte:head>
 
+<p class="text-2xl flex justify-center items-center">Coming Soon</p>
--- src/routes/reports/+page.svelte
+++ src/routes/reports/+page.svelte
@@ -5,3 +5,5 @@
 	<title>House Stats | Reports</title>
 </svelte:head>
 
+<p class="text-2xl flex justify-center items-center">Coming Soon</p>
+
